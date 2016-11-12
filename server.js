b17a90f44e2d82ebadab07e7134e663799dd0db7
ajijoyo <self.ajiejoy@gmail.com>
2016-11-12 14:16:19 +0700

    add form

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+
+  socket.on('chat',function(data){
+  	io.emit('chat',data)
+  	console.log(""+data)
+  })
 });
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
